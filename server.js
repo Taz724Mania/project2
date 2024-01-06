fae383bd0abca8dbdd954cf205289f09071821a3
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-01-06 12:56:58 -0500

    basic setup

---
+++ server.js
@@ -0,0 +1,30 @@
+//Deps
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+
+
+
+//database connect
+const {DATABASE_URL, SECRET, PORT} = process.env
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
+//app object
+const app = express()
+
+//routes
+app.get("/", (req, res) => {
+    res.send("They were, all of them, deceived")
+})
+
+
+//server listener
+app.listen(PORT, () => {
+    console.log(`The one ring to rule them all in ${PORT}`)
+})
